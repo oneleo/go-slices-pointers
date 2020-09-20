79ec32828932b10b2a00ef7ec17e785bd057567b
oneleo <oneleo760823@gmail.com>
2020-09-20 09:49:03 +0800

    Add call by value func

--- README.md
+++ README.md
@@ -3,15 +3,26 @@
 
 ## Slice A 範例
 - 建置一個包含 3 個 string 元素的 slice
-- 呼叫傳遞 pointer 的 func
+- 呼叫傳遞 value 的 func
 
 ```go
 var a = []string{"1", "2", "3"}
 
-changeSliceA(&a)
+changeSliceA(a)
 ```
 
 ## Slice B 範例
+- 建置一個包含 3 個 string 元素的 slice
+- 呼叫傳遞 pointer 的 func
+
+```go
+b := make([]string, len(a))
+copy(b, a)
+
+changeSliceB(&b)
+```
+
+## Slice C 範例
 - 建置一個包含 3 個 string pointer 元素的 slice
 - 呼叫傳遞 value 的 func
 
@@ -19,20 +30,20 @@ changeSliceA(&a)
 s1 := "1"
 s2 := "2"
 //s3 := "3"
-var b = []*string{&s1, &s2, new(string)}
+var c = []*string{&s1, &s2, new(string)}
 
-changeSliceB(b)
+changeSliceC(c)
 ```
 
-## Slice C 範例
+## Slice D 範例
 - 建置一個包含 3 個 string pointer 元素的 slice
 - 呼叫傳遞 pointer 的 func
 
 ```go
-c := make([]*string, len(b))
-copy(c, b)
+d := make([]*string, len(c))
+copy(d, c)
 
-changeSliceC(&c)
+changeSliceD(&d)
 ```
 
 ## 下載 & 執行
@@ -45,7 +56,8 @@ $> go run ./main.go
 
 ## 輸出
 ```sh
-a at 0xc000004480       a = [3 2 3 4]
-b at 0xc0000044a0       b = [0xc0000421f0 0xc000042200 0xc000042210]
-c at 0xc0000044e0       c = [0xc0000421f0 0xc000042200 0xc000042210 0xc000042230]
+a at 0xc000004480       a = [3 2 3]
+b at 0xc0000044a0       b = [3 2 3 4]
+c at 0xc0000044c0       c = [0xc0000421f0 0xc000042200 0xc000042210]
+d at 0xc000004500       d = [0xc0000421f0 0xc000042200 0xc000042210 0xc000042230]
 ```
--- main.go
+++ main.go
@@ -3,40 +3,51 @@ package main
 import "fmt"
 
 func main() {
-	// String slice, and func call by pointer
+	// String slice, and func call by value
 	var a = []string{"1", "2", "3"}
-	changeSliceA(&a)
+	changeSliceA(a)
+
+	// String slice, and func call by pointer
+	b := make([]string, len(a))
+	copy(b, a)
+	changeSliceB(&b)
 
 	// String pointer slice, and func call by value
 	s1 := "1"
 	s2 := "2"
 	//s3 := "3"
-	var b = []*string{&s1, &s2, new(string)}
-	changeSliceB(b)
+	var c = []*string{&s1, &s2, new(string)}
+	changeSliceC(c)
 
 	// String pointer slice, and func call by pointer
-	c := make([]*string, len(b))
-	copy(c, b)
-	changeSliceC(&c)
+	d := make([]*string, len(c))
+	copy(d, c)
+	changeSliceD(&d)
 
 	// Result
 	fmt.Print("a at ", fmt.Sprintf("%p", &a), "\ta = ", a, "\n")
 	fmt.Print("b at ", fmt.Sprintf("%p", &b), "\tb = ", b, "\n")
 	fmt.Print("c at ", fmt.Sprintf("%p", &c), "\tc = ", c, "\n")
+	fmt.Print("d at ", fmt.Sprintf("%p", &d), "\td = ", d, "\n")
 }
 
-func changeSliceA(i *([]string)) {
+func changeSliceA(i []string) {
+	i[0] = "3"
+	i = append(i, "4")
+}
+
+func changeSliceB(i *([]string)) {
 	(*i)[0] = "3"
 	*i = append(*i, "4")
 }
 
-func changeSliceB(i [](*string)) {
+func changeSliceC(i [](*string)) {
 	*(i[0]) = "3"
 	s := "4"
 	i = append(i, &s)
 }
 
-func changeSliceC(i *([](*string))) {
+func changeSliceD(i *([](*string))) {
 	*((*i)[0]) = "3"
 	s := "4"
 	*i = append(*i, &s)
